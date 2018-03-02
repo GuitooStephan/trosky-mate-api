ed0b1c2b38bf69581a3e8ed27d014696de11731a
Stephane Tanoh <guystephane00@gmail.com>
2018-03-02 00:20:48 +0000

    Trying to connect

--- src/main/java/com/gtuc/troskyMate/Application.java
+++ src/main/java/com/gtuc/troskyMate/Application.java
@@ -5,15 +5,28 @@ package com.gtuc.troskyMate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @SpringBootApplication
-@Configuration
+@org.springframework.context.annotation.Configuration
 @EnableAutoConfiguration
 
 
 
 public class Application {
+
+    @Bean
+    public org.neo4j.ogm.config.Configuration getConfiguration() {
+        org.neo4j.ogm.config.Configuration config = new org.neo4j.ogm.config.Configuration();
+        config
+                .driverConfiguration()
+                .setDriverClassName("org.neo4j.ogm.drivers.bolt.driver.BoltDriver")
+                .setURI("bolt://hobby-cjkoaoiekhacgbkeoemilpal.dbs.graphenedb.com:24786")
+                .setCredentials("app89205062-DQZv67", "b.i3LyH7FqZV2T.OyCIVvSXTXvaNon6");
+        return config;
+    }
 
     //Main function of troskyMate
     public static void main(String[] args) throws Exception{
