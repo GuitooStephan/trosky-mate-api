6b68c4cf9cbe59233f3a7f157195bdbc3cfbe69e
guystephane <guystephane00@gmail.com>
2019-06-23 13:09:08 +0000

    add swagger

--- src/main/java/com/gtuc/troskyMate/Application.java
+++ src/main/java/com/gtuc/troskyMate/Application.java
@@ -5,20 +5,26 @@ package com.gtuc.troskyMate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.transaction.Neo4jTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @org.springframework.context.annotation.Configuration
 @EnableAutoConfiguration
 @EnableCaching
+@EnableSwagger2
 public class Application {
+
+    @Bean
+    public Docket troskyMateApi() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors.basePackage("com.gtuc.troskyMate.controllers")).build();
+    }
 
     @Bean
     public org.neo4j.ogm.config.Configuration getConfiguration() {
